fceec46c38900b94527855a097e5bb3461ce1b92
seankiepprien <seankiepprien@msn.com>
2020-11-27 09:53:20 -0500

    deleteTodo undefined

--- js/main.js
+++ js/main.js
@@ -13,9 +13,16 @@ function renderTodo(todo) {
     const table = document.getElementById("mytable");
     // Select the data-key from an item
     const item = document.querySelector(`[data-key='${todo.id}']`);
+    // If block
+    if (todo.deleted) {
+        // Remove the item from the DOM
+        item.remove()
+        return
+    }
     // Use the ternary operator to check if 'todo-item' is true
     // If so, assign 'done' to 'isChecked'. Otherwise, assign an empty string
     const isChecked = todo.checked ? 'done': '';
+    
     // Create a tr element and assign it to 'node'
     const node = document.createElement("tr");
     // Set the class attributes
@@ -24,7 +31,6 @@ function renderTodo(todo) {
     // Set the content of the 'tr' element created above
     node.innerHTML = `
     <th data-key="${todo.id}"><input id="${todo.id}" type="checkbox" /> <label for="${todo.id}"  class="tick js-tick"></label></th>
-    <th scope="row" id="${todo.id}">${itemNum}</th>
     <th scope="row">${todo.text}</th>
     <th><button class="delete-todo js-delete-todo btn"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
@@ -45,18 +51,34 @@ function addTodo(text) {
         text,
         checked: false,
         id: Date.now(),
+        num: 0
     };
+    
     todoItems.push(todo);
     renderTodo(todo);
-    increment(itemNum);
     console.log(todoItems);
 }
+
 
 function toggleDone(key) {
     const index = todoItems.findIndex(item => item.id === Number(key));
     todoItems[index].checked = !todoItems[index].checked;
     renderTodo(todoItems[index]);
   }
+
+function deleteTodo(key) {
+    // Find the corresponding todo object in the todoItems array
+    const index = todoItems.findIndex(item => item.id === Number(key));
+    // Create a new object with properties of the current todo item
+    // and a `deleted` property which is set to true
+    const todo ={
+        deleted: true,
+        ...todoItems[index]
+    };
+    // Remove the todo item from the array by filtering it out
+    todoItems = todoItems.filter(item => item.id !== Number(key));
+    renderTodo(todo);
+}
 
 // Select the form element
 const form = document.querySelector('.myform');
@@ -85,5 +107,10 @@ table.addEventListener('click', event => {
     const itemKey = event.target.parentElement.dataset.key;
     toggleDone(itemKey);
   }
+// If the delete button is clicked
+if (event.target.classList.contains('js-delete-todo')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+}
 });
 
